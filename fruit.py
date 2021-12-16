299eb4f1f1fbbff382e7f8e268fcab397c61b9d5
TouiAndou <74704982+TouiAndou@users.noreply.github.com>
2021-12-16 11:58:54 +0900

    test

--- fruit.py
+++ fruit.py
@@ -1,7 +1,3 @@
 mylist = ["Orange", "Peach", "Lemon", "Apple","Banana","Akebia","Cherry","Kiwi","Lychee"]
 for val in mylist:
     print("value:" + val)
-    #test
-mydict = {"L":"Lemon", "O":"Orage", "G":"Grapes"}
-for mykey, myvalue in mydict.items():
-    print("key:" + mykey + ", value:" + myvalue)
